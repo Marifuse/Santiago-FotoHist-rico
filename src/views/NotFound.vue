b720e995ac138ced4ede69be11fccc36d6b185f0
María Jesús Fuenzalida <maria.fuenzalida.serrano@gmail.com>
2020-07-13 17:37:43 -0400

    Noveno avance con NOTFOUND listo

---
+++ src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not__found">
+    <img class="error__404" src="error-404.png" alt="error-404">
+    <div class="text-center btn__nf">
+      <v-btn rounded color="grey" to="/home">╠ Volver a Home ╣</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.not__found {
+  background-color: #000;
+}
+.error__404 {
+  width: 30em;
+  display: block;
+  margin: auto;
+  margin-top: 3em;
+}
+.btn__nf {
+  margin-bottom: 8.5em;
+}
+</style>
